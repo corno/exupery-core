e526f0262617f1059f69624afbe95935d29e8ed6
Corno Schraverus <corno@schraverus.com>
2025-07-21 23:51:10 +0200

    .

---
+++ packages/exupery-core-data/pub/src/shorthands.ts
@@ -0,0 +1,94 @@
+import * as _ei from 'exupery-core-internals'
+import * as _et from 'exupery-core-types'
+import * as pt from 'exupery-core-types'
+
+export type Raw_Or_Normal_Dictionary<T> = { [key: string]: T } | pt.Dictionary<T>
+export type Raw_Dictionary<T> = { [key: string]: T }
+
+export type Reference_To_Normal_Dictionary_Entry<G_Source, T_Dictionary_Entry> = {
+    readonly 'key': string
+    readonly 'location': G_Source
+}
+
+export type Reference_To_Stacked_Dictionary_Entry<G_Source, T_Dictionary_Entry> = {
+    readonly 'key': string
+    readonly 'location': G_Source
+    readonly 'up steps': number
+}
+
+export const to_raw_array = <T>($: pt.Array<T>): readonly T[] => $.__get_raw_copy()
+
+
+export type Dictionary<G_Source, T_D> = {
+    readonly 'dictionary': _et.Dictionary<{
+        readonly 'entry': T_D
+        readonly 'location': G_Source
+    }>
+    readonly 'location': G_Source
+}
+
+export type List<G_Source, T_L> = {
+    readonly 'list': _et.Array<{
+        readonly 'element': T_L
+        readonly 'location': G_Source
+    }>
+    readonly 'location': G_Source
+}
+
+
+export const wrap_dictionary = <T>($: Raw_Or_Normal_Dictionary<T>): Dictionary<_ei.Source_Location, T> => {
+    const location = _ei.get_location_info(2)
+    function is_normal($: Raw_Or_Normal_Dictionary<T>): $ is pt.Dictionary<T> {
+        return $.to_array !== undefined && typeof $.to_array === "function"
+    }
+    if (is_normal($)) {
+        return {
+            'location': location,
+            'dictionary': $.map(($) => ({
+                'location': location,
+                'entry': $,
+            }))
+        }
+    } else {
+        return {
+            'location': location,
+            'dictionary': _ei.dictionary_literal($).map(($) => ({
+                'location': location,
+                'entry': $,
+            }))
+        }
+    }
+}
+
+export const wrap_list = <T>($: T[]): List<_ei.Source_Location, T> => {
+    const location = _ei.get_location_info(2)
+    return {
+        'location': location,
+        'list': _ei.array_literal($).map(($) => ({
+            'location': location,
+            'element': $,
+        }))
+    }
+}
+
+export const wrap_state_group = <T>($: T) => {
+    return {
+        'location': _ei.get_location_info(2),
+        'state group': $,
+    }
+}
+
+export const wrap_reference = <T>($: string): Reference_To_Normal_Dictionary_Entry<_ei.Source_Location, T> => {
+    return {
+        'location': _ei.get_location_info(2),
+        'key': $,
+    }
+}
+
+export const wrap_stack_reference = <T>(up_steps: number, name: string): Reference_To_Stacked_Dictionary_Entry<_ei.Source_Location, T> => {
+    return {
+        'location': _ei.get_location_info(2),
+        'up steps': up_steps,
+        'key': name,
+    }
+}
