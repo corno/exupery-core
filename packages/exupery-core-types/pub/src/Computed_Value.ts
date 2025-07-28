d543fe397404d9ccb9247b656990a92e4278b45d
Corno Schraverus <corno@schraverus.com>
2025-07-28 19:02:04 +0200

    .

--- packages/exupery-core-types/pub/src/Computed_Value.ts
+++ packages/exupery-core-types/pub/src/Computed_Value.ts
@@ -3,4 +3,6 @@
  * it makes it possible to do the evaluation only when the function is called
  * useful for lazy evaluation or when side effects are needed
  */
-export type Computed_Value<T> = () => T
+export type Computed_Value<T> = {
+    'compute': () => T
+}
