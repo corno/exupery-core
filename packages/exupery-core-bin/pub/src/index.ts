e812984dd7667d577a1f234178ebf97fd5a50d79
Corno Schraverus <corno@schraverus.com>
2025-09-25 10:52:58 +0200

    changed 'run_program'

--- packages/exupery-core-bin/pub/src/index.ts
+++ packages/exupery-core-bin/pub/src/index.ts
@@ -1,5 +1,10 @@
-export * from "./run_program"
+import * as _ei from 'exupery-core-internals'
+import * as _et from 'exupery-core-types'
 
-export * from "./bin_api"
-
-export * from "./bin_glossary"
+export const run_program = (
+    main: ($: _et.Array<string>) => _et.Async_Value<number>
+): void => {
+    main(_ei.array_literal(process.argv.slice(2))).__execute(($) => {
+        process.exitCode = $
+    })
+}
