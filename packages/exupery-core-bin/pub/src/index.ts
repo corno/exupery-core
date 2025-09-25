da213a97efd53a86b07611ea68e300a24b7d636b
Corno Schraverus <corno@schraverus.com>
2025-09-25 21:24:39 +0200

    .

--- packages/exupery-core-bin/pub/src/index.ts
+++ packages/exupery-core-bin/pub/src/index.ts
@@ -1,7 +1,8 @@
 import * as _ei from 'exupery-core-internals'
 import * as _et from 'exupery-core-types'
 
-export type Program_Main = ($: _et.Array<string>) => _et.Async_Value<number>
+export type Program_Main = ($: _et.Array<string>) => _et.Async_Value<null>
+export type Unsafe_Program_Main = ($: _et.Array<string>) => _et.Unsafe_Async_Value<null, number>
 
 /**
  * Runs a program main function, passing command line arguments (excluding
@@ -13,6 +14,17 @@ export const run_program = (
     main: Program_Main
 ): void => {
     main(_ei.array_literal(process.argv.slice(2))).__start(
+        ($) => {
+        }
+    )
+}
+
+export const run_unsafe_program = (
+    main: Unsafe_Program_Main
+): void => {
+    main(_ei.array_literal(process.argv.slice(2))).__start(
+        ($) => {
+        },
         ($) => {
             process.exitCode = $
         }
