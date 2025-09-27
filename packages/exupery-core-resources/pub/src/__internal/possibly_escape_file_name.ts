cdfcc93b2a18d0b413f3867b65ad4dd46341c784
Corno Schraverus <corno@schraverus.com>
2025-09-27 09:59:07 +0200

    .

--- packages/exupery-core-resources/pub/src/__internal/possibly_escape_file_name.ts
+++
@@ -1,6 +0,0 @@
-export const $$ = (path: string, escape: boolean): string => {
-    if (escape) {
-        return path.replaceAll(" ", '_')
-    }
-    return path
-}
--- packages/exupery-core-resources/pub/src/index.ts
+++
@@ -1,1 +0,0 @@
-import * as dummy from "path"
