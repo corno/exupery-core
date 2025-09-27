a57bf71f6ea6d022418ec903405983c7c3d803a7
Corno Schraverus <corno@schraverus.com>
2025-09-27 10:35:11 +0200

    .

--- packages/exupery-core-alg/pub/src/index.ts
+++ packages/exupery-core-alg/pub/src/index.ts
@@ -1,5 +1,5 @@
-import * as pt from 'exupery-core-types'
-import * as pi from "exupery-core-internals"
+import * as _et from 'exupery-core-types'
+import * as _ei from 'exupery-core-internals'
 
 /**
  * these functions coming from core-internals should be exposed for library development
@@ -14,5 +14,41 @@ export {
     array_literal,
     dictionary_literal,
     block,
-    Error
+    Error // <-- remove this one (I think)
 } from "exupery-core-internals"
+
+export const build_list = <T>($: ($c: {
+    'add element': ($: T) => void
+    'add list': ($: _et.Array<T>) => void
+}) => void): _et.Array<T> => {
+    const temp: T[] = []
+    $({
+        'add element': ($) => {
+            temp.push($)
+        },
+        'add list': ($) => {
+            temp.push(...$.__get_raw_copy())
+        }
+    })
+    return _ei.array_literal(temp)
+}
+
+export const build_text = (
+    $c: (
+        $c: {
+            'add snippet': ($: string) => void
+            'add character': ($: number) => void
+        }
+    ) => void
+) => {
+    let out = ""
+    $c({
+        'add snippet': ($) => {
+            out += $
+        },
+        'add character': ($) => {
+            out += String.fromCodePoint($)
+        }
+    })
+    return out
+}
