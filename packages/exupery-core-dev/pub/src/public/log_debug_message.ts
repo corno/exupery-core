c4b164789709a223f57f463ddcc0bcbf9a17d7a6
Corno Schraverus <corno@schraverus.com>
2025-09-29 08:37:55 +0200

    .

--- packages/exupery-core-dev/pub/src/public/log_debug_message.ts
+++ packages/exupery-core-dev/pub/src/public/log_debug_message.ts
@@ -5,6 +5,10 @@
  * before publishing, the usages should be removed
  * @param message the string to be printed to stdout
  */
-export function log_debug_message(...message: string[]): void {
-    console.log("DEBUG", ...message)
+export function log_debug_message<T>(
+    message: string,
+    handler: () => T
+): T {
+    console.log("DEBUG", message)
+    return handler()
 }
