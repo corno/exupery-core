0b66020de1c0ef35f7fcae45df85277c99fe615d
Corno Schraverus <corno@schraverus.com>
2025-09-30 09:49:57 +0200

    .

--- packages/exupery-core-bin/pub/src/index.ts
+++ packages/exupery-core-bin/pub/src/index.ts
@@ -3,14 +3,16 @@ import * as _et from 'exupery-core-types'
 import * as _easync from 'exupery-core-async'
 
 
+export type Runs_Safe_Program_Main = ($: {
+    'arguments': _et.Array<string>
+}) => _easync.Safe_Command_Result
+
 /**
  * Runs a program main function, passing command line arguments (excluding
  * `node` and the script name)
  */
 export const run_program = (
-    main: ($: {
-        'arguments': _et.Array<string>
-    }) => _easync.Safe_Command_Result
+    main: Runs_Safe_Program_Main
 ): void => {
     main({
         'arguments': _ei.array_literal(process.argv.slice(2))
@@ -24,15 +26,17 @@ export type Error = {
     'exit code': number
 }
 
+export type Run_Unsafe_Program_Main = ($: {
+    'arguments': _et.Array<string>
+}) => _easync.Unsafe_Command_Result<Error>
+
 /**
  * Runs a program main function, passing command line arguments (excluding
  * `node` and the script name), and setting the process exit code to the
  * returned value when the async value completes.
  */
 export const run_unsafe_program = (
-    main: ($: {
-        'arguments': _et.Array<string>
-    }) => _easync.Unsafe_Command_Result<Error>
+    main: Run_Unsafe_Program_Main
 ): void => {
     main({
         'arguments': _ei.array_literal(process.argv.slice(2))
