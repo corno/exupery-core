0d6c507836dcf743ee7ab8a73344fb9f489b83c8
Corno Schraverus <corno@schraverus.com>
2025-09-30 11:22:46 +0200

    .

--- packages/exupery-core-bin/pub/src/index.ts
+++ packages/exupery-core-bin/pub/src/index.ts
@@ -3,9 +3,12 @@ import * as _et from 'exupery-core-types'
 import * as _easync from 'exupery-core-async'
 
 
-export type Run_Safe_Program_Main = ($: {
-    'arguments': _et.Array<string>
-}) => _easync.Safe_Command_Result
+export type Run_Safe_Program_Main = (
+    $: {
+        'arguments': _et.Array<string>,
+    },
+    init: _easync.Safe_Command_Result
+) => _easync.Safe_Command_Result
 
 /**
  * Runs a program main function, passing command line arguments (excluding
@@ -14,9 +17,12 @@ export type Run_Safe_Program_Main = ($: {
 export const run_program = (
     main: Run_Safe_Program_Main
 ): void => {
-    main({
-        'arguments': _ei.array_literal(process.argv.slice(2))
-    }).__start(
+    main(
+        {
+            'arguments': _ei.array_literal(process.argv.slice(2))
+        },
+        _easync.initialize_safe_command()
+    ).__start(
         () => {
         }
     )
@@ -26,9 +32,12 @@ export type Error = {
     'exit code': number
 }
 
-export type Run_Unsafe_Program_Main = ($: {
-    'arguments': _et.Array<string>
-}) => _easync.Unsafe_Command_Result<Error>
+export type Run_Unsafe_Program_Main = (
+    $: {
+        'arguments': _et.Array<string>
+    },
+    init: _easync.Unsafe_Command_Result<Error>
+) => _easync.Unsafe_Command_Result<Error>
 
 /**
  * Runs a program main function, passing command line arguments (excluding
@@ -38,9 +47,12 @@ export type Run_Unsafe_Program_Main = ($: {
 export const run_unsafe_program = (
     main: Run_Unsafe_Program_Main
 ): void => {
-    main({
-        'arguments': _ei.array_literal(process.argv.slice(2))
-    }).__start(
+    main(
+        {
+            'arguments': _ei.array_literal(process.argv.slice(2))
+        },
+        _easync.initialize_unsafe_command()
+    ).__start(
         () => {
         },
         ($) => {
