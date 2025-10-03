b6f113d0c357577c6d3aeaf185bf426f6b68a507
Corno Schraverus <corno@schraverus.com>
2025-10-03 23:47:30 +0200

    .

--- packages/exupery-core-bin/pub/src/index.ts
+++ packages/exupery-core-bin/pub/src/index.ts
@@ -3,12 +3,7 @@ import * as _et from 'exupery-core-types'
 import * as _easync from 'exupery-core-async'
 
 
-export type Guaranteed_Main = (
-    $i: _easync.Guaranteed_Procedure_Context,
-    $: {
-        'arguments': _et.Array<string>,
-    },
-) => _easync.Guaranteed_Procedure_Context
+export type Guaranteed_Main = _easync.Guaranteed.Procedure
 
 /**
  * Runs a program main function, passing command line arguments (excluding
@@ -17,27 +12,27 @@ export type Guaranteed_Main = (
 export const run_guaranteed_main_procedure = (
     main: Guaranteed_Main
 ): void => {
-    main(
-        _easync.initialize_guaranteed_procedure_context(),
-        {
-            'arguments': _ei.array_literal(process.argv.slice(2))
-        },
-    ).__start(
-        () => {
-        }
-    )
+    _ei.panic("IMPLEMENT ME")
+    // main(
+    //     _easync.initialize_guaranteed_procedure_context(),
+    //     {
+    //         'arguments': _ei.array_literal(process.argv.slice(2))
+    //     },
+    // ).__start(
+    //     () => {
+    //     }
+    // )
+}
+
+export type Parameters = {
+    'arguments': _et.Array<string>,
 }
 
 export type Error = {
     'exit code': number
 }
 
-export type Unguaranteed_Main = (
-    $i: _easync.Unguaranteed_Procedure_Context<Error>,
-    $: {
-        'arguments': _et.Array<string>
-    },
-) => _easync.Unguaranteed_Procedure_Context<Error>
+export type Unguaranteed_Main = _easync.Unguaranteed.Procedure<Error>
 
 /**
  * Runs a program main function, passing command line arguments (excluding
@@ -47,16 +42,17 @@ export type Unguaranteed_Main = (
 export const run_unguaranteed_main_procedure = (
     main: Unguaranteed_Main
 ): void => {
-    main(
-        _easync.initialize_unguaranteed_procedure_context(),
-        {
-            'arguments': _ei.array_literal(process.argv.slice(2))
-        },
-    ).__start(
-        () => {
-        },
-        ($) => {
-            process.exitCode = $['exit code']
-        }
-    )
+    _ei.panic("IMPLEMENT ME")
+    // main(
+    //     _easync.initialize_unguaranteed_procedure_context(),
+    //     {
+    //         'arguments': _ei.array_literal(process.argv.slice(2))
+    //     },
+    // ).__start(
+    //     () => {
+    //     },
+    //     ($) => {
+    //         process.exitCode = $['exit code']
+    //     }
+    // )
 }
