d499ddccc5d8ea00b2dbf24ba5bed73f9d3e644e
Corno Schraverus <corno@schraverus.com>
2025-10-03 12:42:06 +0200

    .

--- packages/exupery-core-alg/pub/src/unsafe_transaction_result.ts
+++ packages/exupery-core-alg/pub/src/unsafe_transaction_result.ts
@@ -5,45 +5,48 @@ export interface Unsafe_Transformation_Result<T, E> {
      * @param set what to do when the value was set, returns the new type
      * @param not_set  what to do when the value was not set, returns the new type
      */
-    'transform result'<NT>(
+    'process result'<NT>(
         success: ($: T) => NT,
         exception: ($: E) => NT,
     ): NT
 
-    map<NT>(
-        handle_value: ($: T) => NT
-    ): Unsafe_Transformation_Result<NT, E>
+    map<NT, NE>(
+        handle_value: ($: T) => NT,
+        handle_exception: ($: E) => NE
+    ): Unsafe_Transformation_Result<NT, NE>
 }
 
 export namespace transformation {
 
-export const failed = <T, E>(
-    exception: E
-): Unsafe_Transformation_Result<T, E> => {
-    return {
-        'transform result': (success, exception_handler) => {
-            return exception_handler(exception)
-        },
-        'map': <NT>(
-            handle_value: ($: T) => NT
-        ): Unsafe_Transformation_Result<NT, E> => {
-            return failed<NT, E>(exception)
+    export const failed = <T, E>(
+        exception: E
+    ): Unsafe_Transformation_Result<T, E> => {
+        return {
+            'process result': (success, exception_handler) => {
+                return exception_handler(exception)
+            },
+            'map': <NT, NE>(
+                handle_value: ($: T) => NT,
+                handle_exception: ($: E) => NE,
+            ): Unsafe_Transformation_Result<NT, NE> => {
+                return failed<NT, NE>(handle_exception(exception))
+            }
         }
     }
-}
 
-export const successful = <T, E>(
-    value: T
-): Unsafe_Transformation_Result<T, E> => {
-    return {
-        'transform result': (success, exception_handler) => {
-            return success(value)
-        },
-        'map': <NT>(
-            handle_value: ($: T) => NT
-        ): Unsafe_Transformation_Result<NT, E> => {
-            return successful<NT, E>(handle_value(value))
+    export const successful = <T, E>(
+        value: T
+    ): Unsafe_Transformation_Result<T, E> => {
+        return {
+            'process result': (success, exception_handler) => {
+                return success(value)
+            },
+            'map': <NT, NE>(
+                handle_value: ($: T) => NT,
+                handle_exception: ($: E) => NE,
+            ): Unsafe_Transformation_Result<NT, NE> => {
+                return successful<NT, NE>(handle_value(value))
+            }
         }
     }
-}
 }
