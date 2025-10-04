ad00e0e44816b1eb491515931bbe6aeaf8fbe2a1
Corno Schraverus <corno@schraverus.com>
2025-10-04 14:15:27 +0200

    .

--- packages/exupery-core-bin/pub/src/index.ts
+++ packages/exupery-core-bin/pub/src/index.ts
@@ -3,25 +3,20 @@ import * as _et from 'exupery-core-types'
 import * as _easync from 'exupery-core-async'
 
 
-export type Guaranteed_Main = _easync.Guaranteed.Procedure
+export type Initialize_Guaranteed_Main = _easync.Initialize_Guaranteed_Procedure<Parameters>
 
 /**
  * Runs a program main function, passing command line arguments (excluding
  * `node` and the script name)
  */
 export const run_guaranteed_main_procedure = (
-    main: Guaranteed_Main
+    main: Initialize_Guaranteed_Main
 ): void => {
-    _ei.panic("IMPLEMENT ME")
-    // main(
-    //     _easync.initialize_guaranteed_procedure_context(),
-    //     {
-    //         'arguments': _ei.array_literal(process.argv.slice(2))
-    //     },
-    // ).__start(
-    //     () => {
-    //     }
-    // )
+    main({
+        'arguments': _ei.array_literal(process.argv.slice(2))
+    }).__start(
+        () => { }
+    )
 }
 
 export type Parameters = {
@@ -32,7 +27,7 @@ export type Error = {
     'exit code': number
 }
 
-export type Unguaranteed_Main = _easync.Unguaranteed.Procedure<Error>
+export type Initialize_Unguaranteed_Main = _easync.Initialize_Unguaranteed_Procedure<Parameters, Error>
 
 /**
  * Runs a program main function, passing command line arguments (excluding
@@ -40,19 +35,15 @@ export type Unguaranteed_Main = _easync.Unguaranteed.Procedure<Error>
  * returned value when the async value completes.
  */
 export const run_unguaranteed_main_procedure = (
-    main: Unguaranteed_Main
+    main: Initialize_Unguaranteed_Main
 ): void => {
-    _ei.panic("IMPLEMENT ME")
-    // main(
-    //     _easync.initialize_unguaranteed_procedure_context(),
-    //     {
-    //         'arguments': _ei.array_literal(process.argv.slice(2))
-    //     },
-    // ).__start(
-    //     () => {
-    //     },
-    //     ($) => {
-    //         process.exitCode = $['exit code']
-    //     }
-    // )
+    main({
+        'arguments': _ei.array_literal(process.argv.slice(2))
+    }).__start(
+        () => {
+        },
+        ($) => {
+            process.exitCode = $['exit code']
+        }
+    )
 }
