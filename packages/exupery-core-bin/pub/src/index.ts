f71073e1d05e2ffd6f979416dc636ce431e1da4b
Corno Schraverus <corno@schraverus.com>
2025-11-06 08:42:02 +0100

    .

--- packages/exupery-core-bin/pub/src/index.ts
+++ packages/exupery-core-bin/pub/src/index.ts
@@ -3,21 +3,6 @@ import * as _et from 'exupery-core-types'
 import * as _easync from 'exupery-core-async'
 
 
-export type Guaranteed_Main_Initializer = _easync.Guaranteed_Procedure_Initializer<Parameters>
-
-/**
- * Runs a program main function, passing command line arguments (excluding
- * `node` and the script name)
- */
-export const run_guaranteed_main_procedure = (
-    main: Guaranteed_Main_Initializer
-): void => {
-    main({
-        'arguments': _ei.array_literal(process.argv.slice(2))
-    }).__start(
-        () => { }
-    )
-}
 
 export type Parameters = {
     'arguments': _et.Array<string>,
@@ -27,15 +12,26 @@ export type Error = {
     'exit code': number
 }
 
-export type Unguaranteed_Main_Initializer = _easync.Unguaranteed_Procedure_Initializer<Parameters, Error>
-
+/**
+ * Runs a program main function, passing command line arguments (excluding
+ * `node` and the script name)
+ */
+export const run_guaranteed_main_procedure = (
+    main: _easync.Guaranteed_Procedure_Initializer<Parameters>
+): void => {
+    main({
+        'arguments': _ei.array_literal(process.argv.slice(2))
+    }).__start(
+        () => { }
+    )
+}
 /**
  * Runs a program main function, passing command line arguments (excluding
  * `node` and the script name), and setting the process exit code to the
  * returned value when the async value completes.
  */
 export const run_unguaranteed_main_procedure = (
-    main: Unguaranteed_Main_Initializer
+    main: _easync.Unguaranteed_Procedure_Initializer<Parameters, Error>
 ): void => {
     main({
         'arguments': _ei.array_literal(process.argv.slice(2))
