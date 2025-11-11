739a998f7af39eda4b2ac846895329e287104f9b
Corno Schraverus <corno@schraverus.com>
2025-11-11 18:59:29 +0100

    fixed imports

--- packages/exupery-core-bin/pub/src/index.ts
+++ packages/exupery-core-bin/pub/src/index.ts
@@ -16,6 +16,20 @@ import * as d_read_file from "exupery-resources/dist/interface/generated/pareto/
 import * as d_stat from "exupery-resources/dist/interface/generated/pareto/schemas/stat/data_types/target"
 import * as d_read_directory from "exupery-resources/dist/interface/generated/pareto/schemas/read_directory/data_types/target"
 
+import { $$ as p_copy_signature } from "exupery-resources/dist/implementation/algorithms/procedures/unguaranteed/copy"
+import { $$ as p_make_directory } from "exupery-resources/dist/implementation/algorithms/procedures/unguaranteed/make_directory"
+import { $$ as p_stat } from "exupery-resources/dist/implementation/algorithms/queries/unguaranteed/stat"
+import { $$ as p_read_directory } from "exupery-resources/dist/implementation/algorithms/queries/unguaranteed/read_directory"
+import { $$ as p_read_file } from "exupery-resources/dist/implementation/algorithms/queries/unguaranteed/read_file"
+import { $$ as p_execute_any_query_executable } from "exupery-resources/dist/implementation/algorithms/queries/unguaranteed/execute_any_query_executable"
+import { $$ as p_execute_any_procedure_executable } from "exupery-resources/dist/implementation/algorithms/procedures/unguaranteed/execute_any_procedure_executable"
+import { $$ as p_log } from "exupery-resources/dist/implementation/algorithms/procedures/guaranteed/log"
+import { $$ as p_log_error } from "exupery-resources/dist/implementation/algorithms/procedures/guaranteed/log_error"
+import { $$ as p_write_to_stdout } from "exupery-resources/dist/implementation/algorithms/procedures/guaranteed/write_to_stdout"
+import { $$ as p_write_to_stderr } from "exupery-resources/dist/implementation/algorithms/procedures/guaranteed/write_to_stderr"
+import { $$ as p_write_file } from "exupery-resources/dist/implementation/algorithms/procedures/unguaranteed/write_file"
+
+
 
 export type Parameters = {
     'arguments': _et.Array<string>,
@@ -27,10 +41,10 @@ export type Error = {
 
 export type Available_Standard_Resources = {
     'procedures': {
-        'copy': _easync.Guaranteed_Procedure<d_copy.Parameters, null>,
-        'make directory': _easync.Guaranteed_Procedure<d_make_directory.Parameters, null>,
-        'write file': _easync.Guaranteed_Procedure<d_write_file.Parameters, null>,
-        'execute any procedure executable': _easync.Guaranteed_Procedure<d_execute_any_procedure_executable.Parameters, null>,
+        'copy': _easync.Unguaranteed_Procedure<d_copy.Parameters, d_copy.Error, null>,
+        'make directory': _easync.Unguaranteed_Procedure<d_make_directory.Parameters, d_make_directory.Error, null>,
+        'write file': _easync.Unguaranteed_Procedure<d_write_file.Parameters, d_write_file.Error, null>,
+        'execute any procedure executable': _easync.Unguaranteed_Procedure<d_execute_any_procedure_executable.Parameters, d_execute_any_procedure_executable.Error, null>,
         'log': _easync.Guaranteed_Procedure<d_log.Parameters, null>,
         'log error': _easync.Guaranteed_Procedure<d_log_error.Parameters, null>,
         'write to stdout': _easync.Guaranteed_Procedure<d_write_to_stdout.Parameters, null>,
@@ -47,14 +61,14 @@ export type Available_Standard_Resources = {
 const create_available_resources = (): Available_Standard_Resources => {
     return {
         'procedures': {
-            'copy': require('../algorithms/procedures/guaranteed/copy').$$,
-            'make directory': require('../algorithms/procedures/guaranteed/make_directory').$$,
-            'write file': require('../algorithms/procedures/guaranteed/write_file').$$,
-            'execute any procedure executable': require('../algorithms/procedures/guaranteed/execute_any_procedure_executable').$$,
-            'log': require('../algorithms/procedures/guaranteed/log').$$,
-            'log error': require('../algorithms/procedures/guaranteed/log_error').$$,
-            'write to stdout': require('../algorithms/procedures/guaranteed/write_to_stdout').$$,
-            'write to stderr': require('../algorithms/procedures/guaranteed/write_to_stderr').$$,
+            'copy': p_copy_signature,
+            'make directory': p_make_directory,
+            'write file': p_write_file,
+            'execute any procedure executable': p_execute_any_procedure_executable,
+            'log': p_log,
+            'log error': p_log_error,
+            'write to stdout': p_write_to_stdout,
+            'write to stderr': p_write_to_stderr,
         },
         'queries': {
             'execute any query executable': require('../algorithms/queries/unguaranteed/execute_any_query_executable').$$,
