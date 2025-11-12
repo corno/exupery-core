e7c195b7cb30e31884a40c579f6dae283aee7c47
Corno Schraverus <corno@schraverus.com>
2025-11-12 04:14:11 +0100

    fixed function name

--- packages/exupery-core-alg/pub/src/text_to_character_list.ts
+++ packages/exupery-core-alg/pub/src/text_to_character_list.ts
@@ -1,7 +1,7 @@
 import * as _et from 'exupery-core-types'
 import * as _ea from 'exupery-core-internals'
 
-export const $$ = ($: string): _et.Array<number> => {
+export const text_to_character_list = ($: string): _et.Array<number> => {
     const out: number[] = []
     for (let i = 0; i < $.length; i++) {
         out.push($.charCodeAt(i))
