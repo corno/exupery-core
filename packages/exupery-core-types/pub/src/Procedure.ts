ac332aa7f58aa6673df8029d5c20861d795d5915
Corno Schraverus <corno@schraverus.com>
2025-11-14 22:46:08 +0100

    .

--- packages/exupery-core-types/pub/src/Procedure.ts
+++ packages/exupery-core-types/pub/src/Procedure.ts
@@ -1,8 +1,10 @@
+import { Query_Promise } from "./Query"
 
 export type Procedure<Parameters, Error, Resources> = ($r: Resources) => Procedure_Primed_With_Resources<Parameters, Error>
 
 export type Procedure_Primed_With_Resources<Parameters, Error> = {
     'execute with synchrounous data': ($: Parameters) => Procedure_Promise<Error>
+    'execute with asynchrounous data': (query: Query_Promise<Parameters, Error>) => Procedure_Promise<Error>
 }
 
 export type Procedure_Promise<Error> = {
