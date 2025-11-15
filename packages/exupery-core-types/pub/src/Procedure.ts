e3042977a67cc2f4b3fb65a052de667ddf9ca707
Corno Schraverus <corno@schraverus.com>
2025-11-15 16:03:03 +0100

    .

--- packages/exupery-core-types/pub/src/Procedure.ts
+++ packages/exupery-core-types/pub/src/Procedure.ts
@@ -3,8 +3,23 @@ import { Query_Promise } from "./Query"
 export type Procedure<Parameters, Error, Resources> = ($r: Resources) => Procedure_Primed_With_Resources<Parameters, Error>
 
 export type Procedure_Primed_With_Resources<Parameters, Error> = {
-    'execute with synchronous data': ($: Parameters) => Procedure_Promise<Error>
-    'execute with asynchronous data': (query: Query_Promise<Parameters, Error>) => Procedure_Promise<Error>
+    'execute with synchronous data': (
+        parameters: Parameters
+    ) => Procedure_Promise<Error>
+
+    'execute with synchronous data and map error': <New_Error>(
+        parameters: Parameters,
+        map_error: (error: Error) => New_Error,
+    ) => Procedure_Promise<New_Error>
+    
+    'execute with asynchronous data': (
+        query: Query_Promise<Parameters, Error>
+    ) => Procedure_Promise<Error>
+    
+    'execute with asynchronous data and map error': <New_Error>(
+        query: Query_Promise<Parameters, Error>,
+        map_error: (error: Error) => New_Error,
+    ) => Procedure_Promise<New_Error>
 }
 
 export type Procedure_Promise<Error> = {
