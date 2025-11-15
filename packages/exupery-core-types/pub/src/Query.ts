4e694e7edf82fa1f055db00289b113aac06b4f34
Corno Schraverus <corno@schraverus.com>
2025-11-15 12:19:24 +0100

    .

--- packages/exupery-core-types/pub/src/Query.ts
+++ packages/exupery-core-types/pub/src/Query.ts
@@ -1,3 +1,4 @@
+import { Processor } from "./Processor"
 
 
 
@@ -6,18 +7,19 @@ export type Query<Parameters, Result, Error, Resources> = ($r: Resources) => Que
 export type Query_Primed_With_Resources<Parameters, Result, Error> = ($: Parameters) => Query_Promise<Result, Error>
 
 export type Query_Promise<Result, Error> = {
-    map_<NT>(
-        handle_value: ($: Result) => NT
-    ): Query_Promise<NT, Error>
 
-    map_exception_<NE>(
+
+    map_exception<NE>(
         handle_exception: ($: Error) => NE
     ): Query_Promise<Result, NE>
-
 
     then<NT>(
         handle_value: ($: Result) => Query_Promise<NT, Error>
     ): Query_Promise<NT, Error>
+
+    process<New_Result>(
+        processor: Processor<Result, New_Result, Error>
+    ): Query_Promise<New_Result, Error>
 
     __start(
         on_success: ($: Result) => void,
