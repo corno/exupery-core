55050fd39ec688e02370d05160c7dcf55bdd532a
Corno Schraverus <corno@schraverus.com>
2025-11-15 05:47:22 +0100

    .

--- packages/exupery-core-async/pub/src/algorithms/procedure/create_procedure_primed_with_resources.ts
+++ packages/exupery-core-async/pub/src/algorithms/procedure/create_procedure_primed_with_resources.ts
@@ -1,30 +1,7 @@
 import * as _et from 'exupery-core-types'
 import { __create_procedure_promise } from './create_procedure_promise'
 
-export const create_procedure_primed_with_resources_a = <Parameters, Error, Resources>(
-    handler: ($: Parameters) => _et.Procedure_Promise<Error>,
-): _et.Procedure_Primed_With_Resources<Parameters, Error> => {
-    return {
-        'execute with synchronous data': handler,
-        'execute with asynchronous data': (query) => {
-            return __create_procedure_promise({
-                'execute': (on_success, on_exception) => {
-                    query.__start(
-                        ($) => {
-                            handler($).__start(
-                                on_success,
-                                on_exception,
-                            )
-                        },
-                        on_exception,
-                    )
-                }
-            })
-        }
-    }
-}
-
-export const create_procedure_primed_with_resources_b = <Parameters, Error, Resources>(
+export const create_procedure_primed_with_resources = <Parameters, Error, Resources>(
     handler: ($: Parameters) => _et.Procedure_Promise<Error>,
 ): _et.Procedure_Primed_With_Resources<Parameters, Error> => {
     return {
