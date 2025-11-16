35f540cf4c69dc6a7c477a10669f2fd0c8ad077e
Corno Schraverus <corno@schraverus.com>
2025-11-16 09:16:28 +0100

    .

--- packages/exupery-core-async/pub/src/query/creaters/create_query.ts
+++ packages/exupery-core-async/pub/src/query/creaters/create_query.ts
@@ -2,18 +2,7 @@ import * as _et from 'exupery-core-types'
 import { __create_query_promise } from './create_query_promise'
 
 export const __create_query = <Parameters, Result, Error, Resources>(
-    handler: ($: Parameters) => _et.Query_Promise<Result, Error>,
+    handler: _et.Query<Parameters, Result, Error>,
 ): _et.Query<Parameters, Result, Error> => {
-    return {
-        'execute': (parameters) => {
-            return __create_query_promise({
-                'execute': (on_success, on_error) => {
-                    handler(parameters).__start(
-                        on_success,
-                        on_error,
-                    )
-                }
-            })
-        }
-    }
+    return handler
 }
