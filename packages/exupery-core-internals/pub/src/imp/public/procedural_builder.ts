5a01c4f8270fe13c686aea3b86b6bf653658c0eb
Corno Schraverus <corno@schraverus.com>
2025-11-16 14:37:21 +0100

    .

--- packages/exupery-core-internals/pub/src/imp/public/procedural_builder.ts
+++ packages/exupery-core-internals/pub/src/imp/public/procedural_builder.ts
@@ -3,38 +3,38 @@ import { dictionary_literal } from './dictionary_literal'
 import { array_literal } from './array_literal'
 
 export type Procedural_Dictionary_Builder<Entry> = {
-    'add_entry': (key: string, entry: Entry) => void,
-    'get_dictionary': () => _et.Dictionary<Entry>,
+    'add entry': (key: string, entry: Entry) => void,
+    'get dictionary': () => _et.Dictionary<Entry>,
 }
 
 export const create_procedural_dictionary_builder = <Entry>(): Procedural_Dictionary_Builder<Entry> => {
     const entries: { [key: string]: Entry } = {}
 
     return {
-        'add_entry': (key: string, entry: Entry) => {
+        'add entry': (key: string, entry: Entry) => {
             entries[key] = entry
         },
 
-        'get_dictionary': () => {
+        'get dictionary': () => {
             return dictionary_literal(entries)
         },
     }
 }
 
 export type Procedural_List_Builder<Item> = {
-    'add_item': (item: Item) => void,
-    'get_list': () => _et.Array<Item>,
+    'add item': (item: Item) => void,
+    'get list': () => _et.Array<Item>,
 }
 
 export const create_procedural_list_builder = <Item>(): Procedural_List_Builder<Item> => {
     const items: Item[] = []
 
     return {
-        'add_item': (item: Item) => {
+        'add item': (item: Item) => {
             items.push(item)
         },
 
-        'get_list': () => {
+        'get list': () => {
             return array_literal(items)
         },
     }
