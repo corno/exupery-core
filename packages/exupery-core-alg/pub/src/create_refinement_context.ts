cbb6df436d60e402276b384475cc67bec605c0b3
Corno Schraverus <corno@schraverus.com>
2025-11-16 19:56:31 +0100

    .

--- packages/exupery-core-alg/pub/src/create_refinement_context.ts
+++ packages/exupery-core-alg/pub/src/create_refinement_context.ts
@@ -15,7 +15,7 @@ export const create_refinement_context = <Result, Internal_Error, External_Error
     callback: (abort: Abort<Internal_Error>) => Result,
 ): _et.Data_Preparation_Result<Result, External_Error> => {
     try {
-        return _ei.refinement.successful(callback((error) => {
+        return _ei.da.successful(callback((error) => {
             throw new Refine_Guard_Abort_Error(error);
         }))
     } catch (e) {
