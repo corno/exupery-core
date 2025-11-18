b89a3b263012d754c10f9343bd23c17fcbb6c150
Corno Schraverus <corno@schraverus.com>
2025-11-18 15:38:04 +0100

    .

--- packages/exupery-core-async/pub/src/query/creaters/create_query_procedure.ts
+++ packages/exupery-core-async/pub/src/query/creaters/create_query_procedure.ts
@@ -4,8 +4,8 @@ import { __create_query } from './create_query'
 
 export const create_query_procedure = <Result, Error, Parameters, Queries>(
     handler: (
-        $q: Queries,
         $p: Parameters,
+        $q: Queries,
     ) => _et.Data_Preparation_Result<Result, Error>
 ): _et.Query_Procedure<Result, Error, Parameters, Queries> => {
     return ($q) => __create_query(($p) => handler($q, $p))
