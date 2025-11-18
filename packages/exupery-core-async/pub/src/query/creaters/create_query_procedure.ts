a03616912c40b966dd7cc9a1ad244b0051e3d54a
Corno Schraverus <corno@schraverus.com>
2025-11-18 15:38:30 +0100

    .

--- packages/exupery-core-async/pub/src/query/creaters/create_query_procedure.ts
+++ packages/exupery-core-async/pub/src/query/creaters/create_query_procedure.ts
@@ -8,5 +8,5 @@ export const create_query_procedure = <Result, Error, Parameters, Queries>(
         $q: Queries,
     ) => _et.Data_Preparation_Result<Result, Error>
 ): _et.Query_Procedure<Result, Error, Parameters, Queries> => {
-    return ($q) => __create_query(($p) => handler($q, $p))
+    return ($q) => __create_query(($p) => handler($p, $q))
 }
