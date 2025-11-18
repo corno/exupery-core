e195b1da149ce1d978c74cf3a61852fb02903353
Corno Schraverus <corno@schraverus.com>
2025-11-18 06:47:54 +0100

    .

--- packages/exupery-core-async/pub/src/query/creaters/create_query.ts
+++ packages/exupery-core-async/pub/src/query/creaters/create_query.ts
@@ -1,7 +1,7 @@
 import * as _et from 'exupery-core-types'
 
-export const __create_query = <Parameters, Result, Error, Resources>(
-    handler: _et.Data_Preparer<Parameters, Result, Error>,
-): _et.Data_Preparer<Parameters, Result, Error> => {
+export const __create_query = <Result, Parameters, Error, Resources>(
+    handler: _et.Data_Preparer<Result, Parameters, Error>,
+): _et.Data_Preparer<Result, Parameters, Error> => {
     return handler
 }
