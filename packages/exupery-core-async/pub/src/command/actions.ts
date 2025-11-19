c55be01a7b796507249c4e9e059c2600d28a7930
Corno Schraverus <corno@schraverus.com>
2025-11-19 03:19:05 +0100

    .

--- packages/exupery-core-async/pub/src/command/actions.ts
+++ packages/exupery-core-async/pub/src/command/actions.ts
@@ -7,21 +7,22 @@ import { Basic_Command } from './creaters/create_resource_command'
 
 export namespace p {
 
-    export namespace assert {
-        export const direct = <Error>(
-            assertion: boolean,
-            error_if_failed: Error,
-        ): _et.Command_Promise<Error> => {
-            return __create_command_promise({
-                'execute': (on_success, on_error) => {
-                    if (!assertion) {
-                        on_error(error_if_failed)
-                        return
-                    }
-                    on_success()
+    export const assert = <Error>(
+        assertion: boolean,
+        error_if_failed: Error,
+    ): _et.Command_Promise<Error> => {
+        return __create_command_promise({
+            'execute': (on_success, on_error) => {
+                if (!assertion) {
+                    on_error(error_if_failed)
+                    return
                 }
-            })
-        }
+                on_success()
+            }
+        })
+    }
+
+    export namespace deprecated_assert {
 
         export const query = <Error>(
             assertion: _et.Staging_Result<boolean, Error>,
@@ -46,50 +47,52 @@ export namespace p {
         }
     }
 
-    export namespace conditional {
-        export const direct = <Error>(
-            precondition: boolean,
-            procedure: _et.Command_Promise<Error>,
-            else_procedure?: _et.Command_Promise<Error>,
-        ): _et.Command_Promise<Error> => {
-            return __create_command_promise({
-                'execute': (on_success, on_error) => {
-                    if (precondition) {
-                        procedure.__start(
+
+    export const conditional = <Error>(
+        precondition: boolean,
+        command: _et.Command_Promise<Error>,
+        else_command?: _et.Command_Promise<Error>,
+    ): _et.Command_Promise<Error> => {
+        return __create_command_promise({
+            'execute': (on_success, on_error) => {
+                if (precondition) {
+                    command.__start(
+                        on_success,
+                        on_error
+                    )
+                } else {
+                    if (else_command !== undefined) {
+                        else_command.__start(
                             on_success,
                             on_error
                         )
                     } else {
-                        if (else_procedure !== undefined) {
-                            else_procedure.__start(
-                                on_success,
-                                on_error
-                            )
-                        } else {
-                            on_success()
-                        }
+                        on_success()
                     }
                 }
-            })
-        }
+            }
+        })
+    }
+
+    export namespace deprecated_conditional {
 
         export const query = <Error>(
             precondition: _et.Staging_Result<boolean, Error>,
-            procedure: _et.Command_Promise<Error>,
-            else_procedure?: _et.Command_Promise<Error>,
+            command: _et.Command_Promise<Error>,
+            else_command?: _et.Command_Promise<Error>,
         ): _et.Command_Promise<Error> => {
             return __create_command_promise({
                 'execute': (on_success, on_error) => {
                     precondition.__extract_data(
                         ($) => {
                             if ($) {
-                                procedure.__start(
+                                command.__start(
                                     on_success,
                                     on_error
                                 )
                             } else {
-                                if (else_procedure !== undefined) {
-                                    else_procedure.__start(
+                                if (else_command !== undefined) {
+                                    else_command.__start(
                                         on_success,
                                         on_error
                                     )
@@ -104,15 +107,15 @@ export namespace p {
             })
         }
 
-        export const procedure = <Procedure_Input, Error>(
-            precondition: _et.Staging_Result<Procedure_Input, Error>,
-            procedure: Basic_Command<Error, Procedure_Input>, // ($: Procedure_Input) => _et.Command_Promise<Error> (maybe it is better to have the non-basic one here?)
+        export const command = <Command_Input, Error>(
+            precondition: _et.Staging_Result<Command_Input, Error>,
+            command: Basic_Command<Error, Command_Input>, // ($: Command_Input) => _et.Command_Promise<Error> (maybe it is better to have the non-basic one here?)
         ): _et.Command_Promise<Error> => {
             return __create_command_promise({
                 'execute': (on_success, on_error) => {
                     precondition.__extract_data(
                         ($) => {
-                            procedure($).__start(
+                            command($).__start(
                                 on_success,
                                 on_error
                             )
@@ -126,9 +129,10 @@ export namespace p {
     }
 
     export namespace list {
+
         export const parallel = <Error, Element_Error>(
             the_array: _et.List<_et.Command_Promise<Element_Error>>,
-            aggregate_errors: _et.Transformer_Without_Parameters<Error, _et.List<Element_Error>>,
+            errors_aggregator: _et.Transformer_Without_Parameters<Error, _et.List<Element_Error>>,
         ): _et.Command_Promise<Error> => {
             return __create_command_promise({
                 'execute': (
@@ -159,7 +163,7 @@ export namespace p {
                             if (errors.__get_number_of_elements() === 0) {
                                 on_success()
                             } else {
-                                on_error(aggregate_errors(errors))
+                                on_error(errors_aggregator(errors))
                             }
                         }
                     )
@@ -199,53 +203,52 @@ export namespace p {
 
     export namespace dictionary {
 
-        export namespace parallel {
+        export const parallel = <T, Error, Entry_Error>(
+            dictionary: _et.Dictionary<T>,
+            callback: (value: T, key: string) => _et.Command_Promise<Entry_Error>,
+            aggregate_errors: _et.Transformer_Without_Parameters<Error, _et.Dictionary<Entry_Error>>,
+        ): _et.Command_Promise<Error> => {
+            return __create_command_promise({
+                'execute': (
+                    on_success,
+                    on_error,
+                ) => {
 
-            export const direct = <T, Error, Entry_Error>(
-                dictionary: _et.Dictionary<T>,
-                callback: (value: T, key: string) => _et.Command_Promise<Entry_Error>,
-                aggregate_errors: _et.Transformer_Without_Parameters<Error, _et.Dictionary<Entry_Error>>,
-            ): _et.Command_Promise<Error> => {
-                return __create_command_promise({
-                    'execute': (
-                        on_success,
-                        on_error,
-                    ) => {
+                    const errors_builder = _ei.create_procedural_dictionary_builder<Entry_Error>()
 
-                        const errors_builder = _ei.create_procedural_dictionary_builder<Entry_Error>()
+                    create_asynchronous_processes_monitor(
+                        (monitor) => {
+                            dictionary.map(($, key) => {
+                                monitor['report process started']()
 
-                        create_asynchronous_processes_monitor(
-                            (monitor) => {
-                                dictionary.map(($, key) => {
-                                    monitor['report process started']()
-
-                                    callback($, key).__start(
-                                        () => {
-                                            monitor['report process finished']()
-                                        },
-                                        (e) => {
-                                            errors_builder['add entry'](key, e)
-                                            monitor['report process finished']()
-                                        }
-                                    )
-                                })
-                            },
-                            () => {
-                                const errors = errors_builder['get dictionary']()
-                                if (errors.__get_number_of_entries() === 0) {
-                                    on_success()
-                                } else {
-                                    on_error(aggregate_errors(errors))
-                                }
+                                callback($, key).__start(
+                                    () => {
+                                        monitor['report process finished']()
+                                    },
+                                    (e) => {
+                                        errors_builder['add entry'](key, e)
+                                        monitor['report process finished']()
+                                    }
+                                )
+                            })
+                        },
+                        () => {
+                            const errors = errors_builder['get dictionary']()
+                            if (errors.__get_number_of_entries() === 0) {
+                                on_success()
+                            } else {
+                                on_error(aggregate_errors(errors))
                             }
-                        )
-                    }
-                })
-            }
+                        }
+                    )
+                }
+            })
+        }
 
+        export namespace deprecated_parallel {
 
             export const query = <T, Error, Entry_Error>(
-                query: _et.Staging_Result<_et.Dictionary<T>, Error>,
+                staging_result: _et.Staging_Result<_et.Dictionary<T>, Error>,
                 callback: (value: T, key: string) => _et.Command_Promise<Entry_Error>,
                 aggregate_errors: _et.Transformer_Without_Parameters<Error, _et.Dictionary<Entry_Error>>,
             ): _et.Command_Promise<Error> => {
@@ -259,7 +262,7 @@ export namespace p {
 
 
 
-                        query.__extract_data(
+                        staging_result.__extract_data(
                             (dictionary) => {
                                 create_asynchronous_processes_monitor(
                                     (monitor) => {
@@ -331,7 +334,7 @@ export namespace p {
             })
         }
 
-        export const parallel_without_transforming_the_error = <Error, Entry_Error>(
+        export const deprecated_parallel_without_transforming_the_error = <Error, Entry_Error>(
             dictionary: _et.Dictionary<_et.Command_Promise<Entry_Error>>,
         ): _et.Command_Promise<_et.Dictionary<Entry_Error>> => {
             return __create_command_promise({
@@ -400,18 +403,18 @@ export namespace p {
     }
 
 
-    export const prepare_data = <Error, Query_Result>(
-        data_preparation_result: _et.Staging_Result<Query_Result, Error>,
-        command: ($v: Query_Result) => _et.Command_Promise<Error>,
+    export const stage = <Error, Staging_Output>(
+        staging_result: _et.Staging_Result<Staging_Output, Error>,
+        command: ($v: Staging_Output) => _et.Command_Promise<Error>,
     ): _et.Command_Promise<Error> => {
         return __create_command_promise({
             'execute': (
                 on_success,
                 on_error,
             ) => {
-                data_preparation_result.__extract_data(
-                    (query_result) => {
-                        command(query_result).__start(
+                staging_result.__extract_data(
+                    (output) => {
+                        command(output).__start(
                             () => {
                                 on_success()
                             },
@@ -428,20 +431,19 @@ export namespace p {
         })
     }
 
-
-    export const prepare_stacked_data = <Error, Query_Result, Parent_Data>(
-        data_preparation_result: _et.Staging_Result<Query_Result, Error>,
+    export const stage_stacked = <Error, Staging_Output, Parent_Data>(
+        staging_result: _et.Staging_Result<Staging_Output, Error>,
         parent_data: Parent_Data,
-        command: ($v: Query_Result, $parent: Parent_Data) => _et.Command_Promise<Error>,
+        command: ($v: Staging_Output, $parent: Parent_Data) => _et.Command_Promise<Error>,
     ): _et.Command_Promise<Error> => {
         return __create_command_promise({
             'execute': (
                 on_success,
                 on_error,
             ) => {
-                data_preparation_result.__extract_data(
-                    (query_result) => {
-                        command(query_result, parent_data).__start(
+                staging_result.__extract_data(
+                    (output) => {
+                        command(output, parent_data).__start(
                             () => {
                                 on_success()
                             },
--- packages/exupery-core-async/pub/src/query/actions.ts
+++ packages/exupery-core-async/pub/src/query/actions.ts
@@ -4,76 +4,80 @@ import * as _et from 'exupery-core-types'
 
 export namespace q {
 
-    export const dictionary_parallel = <Result, Error, Entry_Error>(
-        dictionary: _et.Dictionary<_et.Staging_Result<Result, Entry_Error>>,
-        aggregate_errors: _et.Transformer_Without_Parameters<Error, _et.Dictionary<Entry_Error>>,
+    export namespace dictionary {
 
-    ): _et.Staging_Result<_et.Dictionary<Result>, Error> => {
-        return _ei.__create_staging_result((on_success, on_error) => {
-            let count_down = dictionary.__get_number_of_entries()
-            let has_errors = false
+        export const parallel = <Result, Error, Entry_Error>(
+            dictionary: _et.Dictionary<_et.Staging_Result<Result, Entry_Error>>,
+            aggregate_errors: _et.Transformer_Without_Parameters<Error, _et.Dictionary<Entry_Error>>,
 
-            const errors: { [key: string]: Entry_Error } = {}
-            const results: { [key: string]: Result } = {}
-            const decrement_and_wrap_up_if_done = () => {
-                count_down -= 1
-                if (count_down === 0) {
-                    if (has_errors) {
-                        on_error(aggregate_errors(_ei.dictionary_literal(errors)))
-                    } else {
-                        on_success(_ei.dictionary_literal(results))
+        ): _et.Staging_Result<_et.Dictionary<Result>, Error> => {
+            return _ei.__create_staging_result((on_success, on_error) => {
+                let count_down = dictionary.__get_number_of_entries()
+                let has_errors = false
+
+                const errors: { [key: string]: Entry_Error } = {}
+                const results: { [key: string]: Result } = {}
+                const decrement_and_wrap_up_if_done = () => {
+                    count_down -= 1
+                    if (count_down === 0) {
+                        if (has_errors) {
+                            on_error(aggregate_errors(_ei.dictionary_literal(errors)))
+                        } else {
+                            on_success(_ei.dictionary_literal(results))
+                        }
                     }
                 }
-            }
-            dictionary.map(($, key) => {
-                $.__extract_data(
-                    ($) => {
-                        results[key] = $
-                        decrement_and_wrap_up_if_done()
-                    },
-                    ($) => {
-                        has_errors = true
-                        errors[key] = $
-                        decrement_and_wrap_up_if_done()
-                    },
-                )
+                dictionary.map(($, key) => {
+                    $.__extract_data(
+                        ($) => {
+                            results[key] = $
+                            decrement_and_wrap_up_if_done()
+                        },
+                        ($) => {
+                            has_errors = true
+                            errors[key] = $
+                            decrement_and_wrap_up_if_done()
+                        },
+                    )
+                })
             })
-        })
-    }
+        }
 
-    export const dictionary_parallel_without_error_aggregation = <Result, Error>(
-        $: _et.Dictionary<_et.Staging_Result<Result, Error>>,
-    ): _et.Staging_Result<_et.Dictionary<Result>, _et.Dictionary<Error>> => {
-        return _ei.__create_staging_result((on_success, on_error) => {
-            let count_down = $.__get_number_of_entries()
-            let has_errors = false
+        export const parallel_without_error_aggregation = <Result, Error>(
+            $: _et.Dictionary<_et.Staging_Result<Result, Error>>,
+        ): _et.Staging_Result<_et.Dictionary<Result>, _et.Dictionary<Error>> => {
+            return _ei.__create_staging_result((on_success, on_error) => {
+                let count_down = $.__get_number_of_entries()
+                let has_errors = false
 
-            const errors: { [key: string]: Error } = {}
-            const results: { [key: string]: Result } = {}
-            const decrement_and_wrap_up_if_done = () => {
-                count_down -= 1
-                if (count_down === 0) {
-                    if (has_errors) {
-                        on_error(_ei.dictionary_literal(errors))
-                    } else {
-                        on_success(_ei.dictionary_literal(results))
+                const errors: { [key: string]: Error } = {}
+                const results: { [key: string]: Result } = {}
+                const decrement_and_wrap_up_if_done = () => {
+                    count_down -= 1
+                    if (count_down === 0) {
+                        if (has_errors) {
+                            on_error(_ei.dictionary_literal(errors))
+                        } else {
+                            on_success(_ei.dictionary_literal(results))
+                        }
                     }
                 }
-            }
-            $.map(($, key) => {
-                $.__extract_data(
-                    ($) => {
-                        results[key] = $
-                        decrement_and_wrap_up_if_done()
-                    },
-                    ($) => {
-                        has_errors = true
-                        errors[key] = $
-                        decrement_and_wrap_up_if_done()
-                    },
-                )
+                $.map(($, key) => {
+                    $.__extract_data(
+                        ($) => {
+                            results[key] = $
+                            decrement_and_wrap_up_if_done()
+                        },
+                        ($) => {
+                            has_errors = true
+                            errors[key] = $
+                            decrement_and_wrap_up_if_done()
+                        },
+                    )
+                })
             })
-        })
+        }
+
     }
 
     export const fixed = <Result, Error>(
