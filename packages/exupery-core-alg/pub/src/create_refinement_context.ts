18e925cdefca04615dcc973ea61128c7c2eac718
Corno Schraverus <corno@schraverus.com>
2025-12-17 15:39:17 +0100

    .

--- packages/exupery-core-alg/pub/src/create_refinement_context.ts
+++ packages/exupery-core-alg/pub/src/create_refinement_context.ts
@@ -10,18 +10,17 @@ class Refine_Guard_Abort_Error<Error> {
 
 export type Abort<Error> = (error: Error) => never
 
-export const create_refinement_context = <Result, Internal_Error, External_Error>(
-    error_transform: (internal_error: Internal_Error) => External_Error,
-    callback: (abort: Abort<Internal_Error>) => Result,
-): _et.Refinement_Result<Result, External_Error> => {
+export const create_refinement_context = <Result, Error>(
+    callback: (abort: Abort<Error>) => Result,
+): _et.Refinement_Result<Result, Error> => {
     try {
         return _ei.__create_success_refinement_result(callback((error) => {
             throw new Refine_Guard_Abort_Error(error);
         }))
     } catch (e) {
         if (e instanceof Refine_Guard_Abort_Error) {
-            return _ei.__create_failure_refinement_result<Result, External_Error>(error_transform(e.error))
+            return _ei.__create_failure_refinement_result<Result, Error>(e.error)
         }
-        throw e
+        throw e // re-throw unexpected errors
     }
 }
