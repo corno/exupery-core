33763f4f9824a7637ea05b40d87e4f039b3094fb
Corno Schraverus <corno@schraverus.com>
2025-12-19 16:29:09 +0100

    .

--- packages/exupery-core-bin/pub/src/algorithms/queries/stat.ts
+++ packages/exupery-core-bin/pub/src/algorithms/queries/stat.ts
@@ -9,7 +9,7 @@ import { Signature } from "exupery-resources/dist/interface/algorithms/queries/s
 import { stat as fs_stat } from "fs"
 import * as t_path_to_text from "exupery-resources/dist/implementation/transformers/path/text"
 
-export const $$: Signature = _easync.__create_query((
+export const $$: _et.Query<d.Result, d.Error, d.Parameters> = _easync.__create_query((
     $p
 ) => {
     const __possibly_escape_filename = (path: string, escape: boolean): string => {
