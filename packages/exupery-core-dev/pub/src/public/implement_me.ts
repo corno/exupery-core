a8005858a42a16d5d85f86c5c1e869cb1de24fef
Corno Schraverus <corno@schraverus.com>
2025-12-21 10:33:10 +0100

    .

--- packages/exupery-core-dev/pub/src/public/implement_me.ts
+++ packages/exupery-core-dev/pub/src/public/implement_me.ts
@@ -7,6 +7,6 @@ import { get_location_info, location_to_string } from '../get_location_info'
  * before publishing, the usages should be removed
  * @param message the string to be printed to stderr
  */
-export function implement_me(): never {
-    throw new Error(`IMPLEMENT ME @ ${location_to_string(get_location_info(1))}`)
+export function implement_me(marker: string): never {
+    throw new Error(`IMPLEMENT ME ${marker ? `: '${marker}'` : ''}@ ${location_to_string(get_location_info(1))}`)
 }
